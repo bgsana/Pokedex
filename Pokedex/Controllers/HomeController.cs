c3dd029b9f3d7f3ccddd7d28924faac8ff09d0a1
bgsana <bgs.ana08@gmail.com>
2025-09-08 22:35:02 -0300

    5º Commit - Finalizado a Área Pública

--- Pokedex/Controllers/HomeController.cs
+++ Pokedex/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Pokedex.Data;
 using Pokedex.Models;
+using Pokedex.ViewModels;
 
 namespace Pokedex.Controllers;
 
@@ -11,7 +12,7 @@ public class HomeController : Controller
     private readonly ILogger<HomeController> _logger;
     private readonly AppDbContext _db;
 
-    public HomeController(ILogger<HomeController> logger, 
+    public HomeController(ILogger<HomeController> logger,
     AppDbContext db)
     {
         _logger = logger;
@@ -20,18 +21,37 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
-        var pokemons = _db.Pokemons
-            .Include(p => p.Regiao)
-            .Include(p => p.Genero)
-            .Include(p => p.Tipos)
-            .ThenInclude(t => t.Tipo)
-            .ToList();
-        return View(pokemons);
+        HomeVM home = new()
+        {
+            Tipos = _db.Tipos.ToList(),
+            Pokemons = _db.Pokemons
+                .Include(p => p.Tipos)
+                .ThenInclude(t => t.Tipo)
+                .ToList()
+        };
+        return View(home);
     }
 
-    public IActionResult Details (uint id)
+    public IActionResult Details(uint id)
     {
-        return View();
+        Pokemon pokemon = _db.Pokemons
+                            .Where(p => p.Numero == id)
+                            .Include(p => p.Regiao)
+                            .Include(p => p.Genero)
+                            .Include(p => p.Tipos)
+                            .ThenInclude(t => t.Tipo)
+                            .SingleOrDefault();
+        DetailVM detail = new()
+        {
+            Atual = pokemon,
+            Anterior = _db.Pokemons
+                .OrderByDescending(p => p.Numero)
+                .FirstOrDefault(p => p.Numero < id),
+            Proximo = _db.Pokemons
+                .OrderByDescending(p => p.Numero)
+                .FirstOrDefault(p => p.Numero > id)
+        };
+        return View(detail);
     }
 
     public IActionResult Privacy()
